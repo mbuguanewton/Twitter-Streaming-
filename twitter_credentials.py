e0bce770ba266b20b1d91e0fabb38785bba976c4
Newton <mymbugua@gmail.com>
2018-07-19 22:10:15 +0300

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,5 +1,5 @@
 # variables that contain the user credentials to access the twitter API
-ACCESS_TOKEN = "697338751320772608-iO33TJBfSnxgHhO6TZt1FZZiXQXl29S"
-ACCESS_TOKEN_SECRET = "Vx4k6jMjI3XIf8kNxqWvxPGbftDEGqTdrA7FmVxjrLm9h"
-CONSUMER_KEY = "1s6CzJfXteuJmVFhIWslbyABy"
-CONSUMER_SECRET ="wAj819peE9QkXSWMLp2ODTEv1vP1aV99N1AiTG557EIxjyqQDF"
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
